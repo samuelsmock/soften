9d0c0eca38f200d0d2f2090a49d0f86e0a2fdeb5
Samuel Smock <smock.samuel@gmail.com>
2020-10-18 17:11:02 -0500

    Updates

---
+++ precache-manifest.deeee58416cc1c640d249bbacc5a0d80.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f97ca15b7553dcf671ad751870c80e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb8814b9ec41a00bb3c",
+    "url": "/static/css/main.82c092fc.chunk.css"
+  },
+  {
+    "revision": "150c4d51da79cf36b8e8",
+    "url": "/static/js/2.98289ce6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.98289ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8814b9ec41a00bb3c",
+    "url": "/static/js/main.4bf99efb.chunk.js"
+  },
+  {
+    "revision": "d7f928f3f524c9129da3",
+    "url": "/static/js/runtime-main.36503f1f.js"
+  },
+  {
+    "revision": "b4b0d7a2bc9b9f252290f810fd69c1b4",
+    "url": "/static/media/beach.b4b0d7a2.jpg"
+  },
+  {
+    "revision": "bd146c6d67738a139fd2599e66dc9c9a",
+    "url": "/static/media/candle.bd146c6d.jpg"
+  },
+  {
+    "revision": "8a0449e16d9d4e1ff262894fe559d507",
+    "url": "/static/media/headshot.8a0449e1.jpg"
+  },
+  {
+    "revision": "b4b34f88950f3ab092f97d442756eac1",
+    "url": "/static/media/nw_trail.b4b34f88.jpg"
+  },
+  {
+    "revision": "49754a76f5b405b640655c2a9fd8df6f",
+    "url": "/static/media/sky.49754a76.jpg"
+  },
+  {
+    "revision": "4cea03a305b9bad90835479e739ec0c0",
+    "url": "/static/media/soften_ibm.4cea03a3.png"
+  }
+]);
