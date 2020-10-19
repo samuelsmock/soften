ca35084737f3b5fd984353f6fe45aeff06ce3c5b
Samuel Smock <smock.samuel@gmail.com>
2020-10-18 19:03:12 -0500

    Updates

---
+++ precache-manifest.f01d00cca6ed704e06cc9468c2a5be40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072ca02cefffc3a6092480c94535f7aa",
+    "url": "/soften/index.html"
+  },
+  {
+    "revision": "1074802d3593cb75dfd3",
+    "url": "/soften/static/css/main.beee1356.chunk.css"
+  },
+  {
+    "revision": "9c4701608cbb71a691b5",
+    "url": "/soften/static/js/2.0974ccc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/soften/static/js/2.0974ccc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1074802d3593cb75dfd3",
+    "url": "/soften/static/js/main.ce0f92ae.chunk.js"
+  },
+  {
+    "revision": "4c54fa3913115e6121ab",
+    "url": "/soften/static/js/runtime-main.4d3681f6.js"
+  },
+  {
+    "revision": "b4b0d7a2bc9b9f252290f810fd69c1b4",
+    "url": "/soften/static/media/beach.b4b0d7a2.jpg"
+  },
+  {
+    "revision": "bd146c6d67738a139fd2599e66dc9c9a",
+    "url": "/soften/static/media/candle.bd146c6d.jpg"
+  },
+  {
+    "revision": "8a0449e16d9d4e1ff262894fe559d507",
+    "url": "/soften/static/media/headshot.8a0449e1.jpg"
+  },
+  {
+    "revision": "b4b34f88950f3ab092f97d442756eac1",
+    "url": "/soften/static/media/nw_trail.b4b34f88.jpg"
+  },
+  {
+    "revision": "49754a76f5b405b640655c2a9fd8df6f",
+    "url": "/soften/static/media/sky.49754a76.jpg"
+  },
+  {
+    "revision": "4cea03a305b9bad90835479e739ec0c0",
+    "url": "/soften/static/media/soften_ibm.4cea03a3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samuelsmock.github.io/soften/precache-manifest.c4a74caa06b653022820225967a83f08.js"
+  "/soften/precache-manifest.f01d00cca6ed704e06cc9468c2a5be40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/samuelsmock.github.io/soften/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/soften/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
